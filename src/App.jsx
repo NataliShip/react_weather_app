79f54ff5e8c38ff7013761932ea89e8397193bfd
NataliShip <Natulechka_m.25@mail.ru>
2017-10-01 19:11:04 +0300

    Интеграция с API сервисами погоды и определения города по IP

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,52 @@ import './App.css';
 
 class App extends Component {
 	state = {
-		city: 'Kirov',
-		temperature: '12'
+		city: '',
+		temperature: '',
+		fetching: true
+	}
+
+	componentDidMount() {
+		this.fetchIP();
+	}
+
+	fetchWeatherData = city => {
+		const baseUrl = `http://api.openweathermap.org`;
+		const path = `/data/2.5/weather`;
+		const appId = `0ce0696e903175c9d64f88fccfa096a9`;
+		const query = `units=metric&lang=ru&appid=${appId}`;
+
+		fetch(`${baseUrl}${path}?q=${city}&${query}`)
+			.then(response => response.json())
+			.then(data => {
+				const date = new Date();
+				const time = date.getHours();
+				let minutes = date.getMinutes();
+				if (minutes<10) {minutes = '0'+ minutes}
+
+				this.setState({
+					time,
+					minutes,
+					city,
+					temperature: Math.round(data.main.temp),
+					fetching: false
+				});
+			})
+			.catch(error => console.error(error));
+	}
+
+	fetchIP = () => {
+		fetch('//freegeoip.net/json/')
+			.then(response => response.json())
+			.then(({city}) => this.fetchWeatherData(city))
+			.catch(error => console.log(error));
 	}
 
 	render() {
-		const { city, temperature } = this.state;
-		return (
+		const { city, temperature, fetching } = this.state;
+		return fetching ?
+			<div className="app">Загрузка...</div>
+			:
 			<div className="app">
 				<div className="widget">
 					<div>Погода сегодня</div>
@@ -19,8 +58,7 @@ class App extends Component {
 						temperature={temperature}
 						city={city}/>
 				</div>
-			</div>
-		)
+			</div>;
 	}
 
 }
